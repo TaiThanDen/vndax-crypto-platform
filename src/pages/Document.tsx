146f3ceb0b674def536ec0239f85ff6b8a1519ab
Shilily <tai25062006z@gmail.com>
2025-05-30 16:30:04 +0700

    resolve merge conflict

--- src/pages/Document.tsx
+++ src/pages/Document.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import Navbar from "@/components/Navbar.tsx";
-import Footer from '@/components/Footer';
-import Sidebar from'@/components/Sidebar';
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import Sidebar from "@/components/Sidebar";
 
-const Document = ()=>{
-    return (
-        <div className="min-h-screen bg-vndax-black text-white">
-            <Navbar/>
-            <Sidebar/>
-            <Footer/>
-        </div>
-    );
-}
+const Document = () => {
+  return (
+    <div className="min-h-screen bg-vndax-black text-white">
+      <Sidebar />
+    </div>
+  );
+};
 export default Document;
