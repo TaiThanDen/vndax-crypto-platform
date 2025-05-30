7a8fdf99aa1f7cf388e364a21a65310dc41a04d6
Shilily <tai25062006z@gmail.com>
2025-05-30 16:15:23 +0700

    update domain to market

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -48,36 +48,42 @@ const Navbar: React.FC = () => {
             </a>
           </Link>
           <div className="hidden md:flex items-center space-x-8">
-            <a
-              href="/"
+            <Link
+              to="/"
               className="text-white hover:text-vndax-green transition-colors"
             >
               {t("home")}
-            </a>
-            <a
-              href="#trading"
+            </Link>
+            <Link
+              to="/market"
+              className="text-white hover:text-vndax-green transition-colors"
+            >
+              Thị trường
+            </Link>
+            <Link
+              to="#trading"
               className="text-white hover:text-vndax-green transition-colors"
             >
               {t("trading")}
-            </a>
-            <a
-              href="#security"
+            </Link>
+            <Link
+              to="#security"
               className="text-white hover:text-vndax-green transition-colors"
             >
               {t("security")}
-            </a>
-            <a
-              href="#knowledge"
+            </Link>
+            <Link
+              to="#knowledge"
               className="text-white hover:text-vndax-green transition-colors"
             >
               {t("knowledge")}
-            </a>
-            <a
-              href="#support"
+            </Link>
+            <Link
+              to="#support"
               className="text-white hover:text-vndax-green transition-colors"
             >
               {t("support")}
-            </a>
+            </Link>
           </div>
         </div>
 
